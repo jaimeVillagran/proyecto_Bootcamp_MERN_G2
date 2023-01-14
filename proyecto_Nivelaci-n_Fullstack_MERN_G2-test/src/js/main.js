9bc1e4f154fcd75d08072520d1c04ca355cb8f13
TJaime <jaime.villagran@gmail.com>
2023-01-13 22:03:56 -0300

    cambios en archivos CSS

--- proyecto_Nivelaci-n_Fullstack_MERN_G2-test/src/js/main.js
+++
@@ -1,9 +0,0 @@
-//Import hash router
-import { router } from "./router/index.js";
-
-const principalFunction = () => {
-	location.hash = "home";
-};
-
-window.addEventListener("load", principalFunction);
-window.addEventListener("hashchange", () => router(location.hash));
